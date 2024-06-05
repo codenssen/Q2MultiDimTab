1ef5da7fd4e6598808be25f23908d2d2969a9e72
ABT Aurelien <aurelien@abrassart.net>
2024-06-05 14:56:05 +0200

    Ajoutez des fichiers projet.

---
+++ Q2MultiDimTab/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[,] firstArray = new int[3, 3]
+            {
+                {1,2,3},
+                {2,3,4},
+                {3,4,5},
+            };
+
+        int[][] secondArray = [new int[3], new int[2], new int[1]];
+
+        for (int i = 0; i < 3; i++)
+        {
+            secondArray[0][i] = firstArray[0, i];           
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            secondArray[1][i] = firstArray[1, i + 1];
+        }
+        for (int i = 0; i < 1; i++)
+        {
+            secondArray[2][i] = firstArray[2, i + 2];       
+        }
+
+        foreach (var item in secondArray)
+        {
+            foreach(var number in item)
+            {
+                Console.Write(number);
+                    
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
